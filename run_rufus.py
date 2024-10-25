0b327127291fc3a6c038d58de0f84cc0949b2a18
Kissan Hari <143542844+KissanHariSV@users.noreply.github.com>
2024-10-25 14:10:56 -0500

    Update run_rufus.py

--- run_rufus.py
+++ run_rufus.py
@@ -4,16 +4,16 @@ def main():
     url = input("Please enter the URL to scrape: ")
 
     try:
-
         client = RufusClient()
 
         print("\nHere are all the links on the page:")
+        client.scrape_and_fetch_links(url)  
+        
         prompt = input("Enter the number of the link you want to summarize: ")
 
         filename = input("Enter the filename (without extension) for the summarized file: ")
         file_format = input("Enter the file format to save (json/csv): ").strip().lower()
 
-    
         client.scrape_and_summarize(url, prompt, file_format, filename)
 
     except Exception as e:
